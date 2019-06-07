27aa7169751d9e41d1f9f93fe2dbae112341c362
Joe Baker <joesbaker@gmail.com>
2019-06-07 14:47:58 +0100

    assetPrefix config – does this help?

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     }
-  }
+  },
+  assetPrefix: isProd ? 'http://cpd.weareconvivio.com' : ''
 }
